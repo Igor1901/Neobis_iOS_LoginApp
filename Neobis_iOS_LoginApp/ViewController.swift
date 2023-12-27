6fb05c6e2484c3711bfd13a86908fe96abe6c808
Igor Pachkin <altair1901@icloud.com>
2023-12-27 15:43:51 +0100

    created a project

---
+++ Neobis_iOS_LoginApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Neobis_iOS_LoginApp
+//
+//  Created by Игорь Пачкин on 26/12/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
