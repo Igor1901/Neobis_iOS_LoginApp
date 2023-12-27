d8e2cd7557c9006ff4264ec8ef8302669e245f56
Igor Pachkin <altair1901@icloud.com>
2023-12-27 16:59:23 +0100

    interface builder update

--- Neobis_iOS_LoginApp/ViewController.swift
+++ Neobis_iOS_LoginApp/ViewController.swift
@@ -9,11 +9,23 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var myImage: UIImageView!
+    @IBOutlet weak var welcomeLabel: UILabel!
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var viewPasswordButton: UIButton!
+    @IBOutlet weak var forgotLabell: UILabel!
+    @IBOutlet weak var loginButton: UIButton!
+    @IBOutlet weak var registerLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        configureUI()
     }
 
+    private func configureUI(){
+
+    }
 
 }
 
