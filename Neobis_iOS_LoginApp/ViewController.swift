097ba75f77062e4bb889bd304f250980a1089946
Igor Pachkin <altair1901@icloud.com>
2023-12-28 17:14:26 +0100

    added functionality

--- Neobis_iOS_LoginApp/ViewController.swift
+++ Neobis_iOS_LoginApp/ViewController.swift
@@ -14,18 +14,34 @@ class ViewController: UIViewController {
     @IBOutlet weak var emailTextField: UITextField!
     @IBOutlet weak var passwordTextField: UITextField!
     @IBOutlet weak var viewPasswordButton: UIButton!
-    @IBOutlet weak var forgotLabell: UILabel!
+    @IBOutlet weak var forgotButton: UIButton!
     @IBOutlet weak var loginButton: UIButton!
-    @IBOutlet weak var registerLabel: UILabel!
+    @IBOutlet weak var box: UIView!
+    @IBOutlet weak var accountLabel: UILabel!
+    @IBOutlet weak var registerButton: UIButton!
+
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        configureUI()
     }
 
-    private func configureUI(){
-
+    @IBAction func eyeButtonTapped(_ sender: Any){
+        print("Eye: click click")
     }
-
+    
+    @IBAction func forgotButtonTapped(_ sender: Any){
+        print("Forgot button: click click")
+    }
+    
+    @IBAction func signInButtonTapped(_ sender: Any) {
+        if let username = emailTextField.text, !username.isEmpty,
+           let password = passwordTextField.text, !password.isEmpty {
+            print("Username: \(username)")
+            print("Password: \(password)")
+        }
+    }
+    @IBAction func registerInButtonTapped(_ sender: Any){
+        print("Register button: click click")
+    }
 }
 
